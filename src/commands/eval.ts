8bdb78df04fe9121f36ba272f966f7676fd827c0
Martin Kröner <kronerm9@gmail.com>
2020-12-27 21:35:10 +0100

    Drastically improve eval command

--- src/commands/eval.ts
+++ src/commands/eval.ts
@@ -1,6 +1,7 @@
 import Discord from "discord.js";
 import BetterEmbed from "../util/betterembed.js";
 import DiscordCommand from "./base.js";
+import js_beautify from "js-beautify";
 
 export default class EvalCommand implements DiscordCommand {
   public static readonly id = "eval";
@@ -18,23 +19,44 @@ export default class EvalCommand implements DiscordCommand {
     }
 
     const evalQuery = args.join(" ");
-    const codeBlock = `\`\`\`ts\n${evalQuery}\`\`\`\n`;
+
+    if (evalQuery.includes("setTimeout") || evalQuery.includes("setInterval")) {
+      await message.channel.send(
+        new BetterEmbed()
+          .setAuthor(message.author)
+          .setError(
+            "Cannot use `setTimeout` or `setInterval` inside an eval!\n" +
+              "Use special `await sleep(ms)` instead!"
+          )
+          .setType("recyclable")
+      );
+      return;
+    }
+
+    const codeBlock = `\`\`\`ts\n${js_beautify(evalQuery)}\`\`\`\n`;
+
+    const evalEmbed = new BetterEmbed()
+      .setAuthor(message.author)
+      .setInfo(codeBlock)
+      .setType("recyclable");
+    const evalMsg = await message.channel.send(evalEmbed);
 
     try {
-      const evalResult = await eval(`(async () => { return ${evalQuery} })()`);
-      await message.channel.send(
-        new BetterEmbed()
-          .setAuthor(message.author)
-          .setSuccess(codeBlock + evalResult)
-          .setType("recyclable")
+      async function sleep(ms: number) {
+        return new Promise<void>((resolve) => {
+          setTimeout(() => {
+            resolve();
+          }, ms);
+        });
+      }
+      const evalResult = await eval(`(async()=>{${evalQuery}})()`);
+      await evalMsg.edit(
+        evalEmbed.setSuccess(
+          codeBlock + `**Output**\n\`\`\`ts\n${evalResult}\`\`\``
+        )
       );
     } catch (e) {
-      await message.channel.send(
-        new BetterEmbed()
-          .setAuthor(message.author)
-          .setError(codeBlock + e)
-          .setType("recyclable")
-      );
+      await evalMsg.edit(evalEmbed.setError(codeBlock + e));
     }
   }
 }
