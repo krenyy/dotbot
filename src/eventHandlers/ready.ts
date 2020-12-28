b40f8d18a0c38a63eeb5274111637bf8ce990163
Martin Kröner <kronerm9@gmail.com>
2020-12-28 14:29:44 +0100

    make all bot messages recyclable after startup/restart

--- src/eventHandlers/ready.ts
+++ src/eventHandlers/ready.ts
@@ -3,6 +3,7 @@ import CommandHandler from "./custom/commandHandler.js";
 import TempChannelHandler from "./custom/tempChannelHandler.js";
 import HelpCommand from "../commands/help.js";
 import Discord from "discord.js";
+import BetterEmbed from "../util/betterembed.js";
 
 declare module "discord.js" {
   interface Client {
@@ -20,6 +21,19 @@ export default class ReadyEventHandler {
     });
 
     await TempChannelHandler.initialCleanup();
+
+    /** Make all bot messages recyclable */
+    for (const [, channel] of client.channels.cache) {
+      if (channel.type !== "text") continue;
+      const textChannel = channel as Discord.TextChannel;
+      for (const [, message] of await textChannel.messages.fetch()) {
+        if (message.author !== client.user) continue;
+        if (message.system) continue;
+        await message.edit(
+          new BetterEmbed(message.embeds[0]).setType("recyclable")
+        );
+      }
+    }
 
     console.log(`${client.user.tag} is ready to go!`);
   }
