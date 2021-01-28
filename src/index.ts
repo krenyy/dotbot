b936c3ad52ac85702bb12a99407fa2ed816e33d1
Martin Kröner <kronerm9@gmail.com>
2021-01-28 22:13:50 +0100

    print interrupt signal and exit code

--- src/index.ts
+++ src/index.ts
@@ -5,28 +5,26 @@ import MessageReactionAddEventHandler from "./eventHandlers/messageReactionAdd.j
 import VoiceStateUpdateEventHandler from "./eventHandlers/voiceStateUpdate.js";
 import ReadyEventHandler from "./eventHandlers/ready.js";
 
+function onInterrupt(signal: NodeJS.Signals) {
+  console.log(`Process interrupted by ${signal}`);
+  process.exit(0);
+}
+
 process
   .on("unhandledRejection", (reason, promise) => {
     console.error(`[UNHANDLED REJECTION] ${reason}`);
     console.log(promise);
   })
-  .on("SIGINT", (signal) => {
-    console.log(signal);
-    process.exit(0);
-  })
-  .on("SIGTERM", (signal) => {
-    console.log(signal);
-    process.exit(0);
-  })
+  .on("SIGINT", onInterrupt)
+  .on("SIGTERM", onInterrupt)
   .on("exit", (code) => {
-    console.log(code);
+    console.log(`Exit code: ${code}`);
     client.destroy();
   });
 
 const client = new Discord.Client({
   partials: ["MESSAGE", "CHANNEL", "REACTION"],
 })
-  // Logging
   //.on("debug", (info) => console.debug(`[DEBUG] ${info}`))
   .on("error", (error) => console.error(`[ERROR] ${error}`))
   .on("warn", (warning) => console.warn(`[WARNING] ${warning}`))
